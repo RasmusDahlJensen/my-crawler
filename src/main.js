eb4a88cb5e10e307bda56cbd12c998ec21b3a4f5
Rasmus Dahl Jensen <62021203+RasmusDahlJensen@users.noreply.github.com>
2023-11-18 17:30:24 +0100

    Grab all titles from anchor locations

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import { CheerioCrawler } from "crawlee";
+import { URL } from "node:url";
 
 const crawler = new CheerioCrawler({
-	async requestHandler({ $, request }) {
+	maxRequestsPerCrawl: 20,
+	async requestHandler({ request, $ }) {
 		const title = $("title").text();
 		console.log(`The title of "${request.url}" is: ${title}.`);
+
+		const links = $("a[href]")
+			.map((_, el) => $(el).attr("href"))
+			.get();
+
+		const absoluteUrls = links.map(
+			(link) => new URL(link, request.loadedUrl).href
+		);
+
+		await crawler.addRequests(absoluteUrls);
 	},
 });
 
-await crawler.run(["https://crawlee.dev"]);
+await crawler.run([
+	"https://www.jobindex.dk/jobsoegning/it/systemudvikling/danmark",
+]);
